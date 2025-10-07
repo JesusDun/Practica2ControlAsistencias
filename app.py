7438b6dfe7ebaccb0cf90de07520b8a2f4f48499
JesusDun <alejandrovielmagarcia@gmail.com>
2025-10-07 11:35:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,11 +50,15 @@ def pusherDepartamentos():
 # =========================================================================
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def landingPage():
+    return render_template("landing-page.html")
 
-@app.route("/app")
-def app2():
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/login")
+def appLogin():
     return render_template("login.html")
 
 @app.route("/iniciarSesion", methods=["POST"])
